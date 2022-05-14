c03747a00639fba545fd3a77d50113c8ede94f23
mubarakmohin1 <chemistmubarak1@gmail.com>
2022-05-14 17:48:25 +0600

    booking appointment successful

--- index.js
+++ index.js
@@ -17,9 +17,13 @@ const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run(){
+
+
+  
 try{
     await client.connect();
     const serviceCollection = client.db('doctors_portal').collection('services');
+    const bookingCollection = client.db('doctors_portal').collection('bookings');
     
 app.get('/service',async(req,res)=>{
     const query = {};
@@ -27,6 +31,22 @@ app.get('/service',async(req,res)=>{
     const services = await cursor.toArray();
     res.send(services);
 })
+
+ /**
+     * API Naming Convention
+     * app.get('/booking') // get all bookings in this collection. or get more than one or by filter
+     * app.get('/booking/:id') // get a specific booking 
+     * app.post('/booking') // add a new booking
+     * app.patch('/booking/:id) //
+     * app.delete('/booking/:id) //
+    */
+
+ app.post('/booking', async(req,res)=>{
+   const booking = req.body;
+   const query = {treatment:booking.treatment,date: booking.date,patient:booking.patient}
+   const result = await bookingCollection.insertOne(booking);
+   res.send(result);
+ })
 
 }
 finally{
